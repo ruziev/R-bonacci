4f2c0e8ba09d4456dcd5e58015b6d1e94a9c5b94
Ruziev <jjruziev@gmail.com>
2017-06-11 12:18:01 +0300

    Initial Commit

---
+++ R-bonacci/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  R-bonacci
+//
+//  Created by Ruziev on 6/11/17.
+//  Copyright © 2017 Ruziev. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
